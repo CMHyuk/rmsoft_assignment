7a7f0b268ddf604629223dec1925a7430840c848
CMHyuk <alsgur9710@naver.com>
2023-11-30 21:35:15 +0900

    feat : 회원가입 dto

---
+++ src/main/java/com/rmsoft/assignment/request/MemberSaveRequest.java
@@ -0,0 +1,17 @@
+package com.rmsoft.assignment.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class MemberSaveRequest {
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상, 특수문자를 포함해야 합니다.")
+    private String password;
+
+    private String passwordConfirm;
+}
