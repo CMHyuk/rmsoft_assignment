e09b8e1bc761976e2c4fc464933e92e10afff4f5
CMHyuk <alsgur9710@naver.com>
2023-11-30 21:35:50 +0900

    feat : 회원가입 API

---
+++ src/main/java/com/rmsoft/assignment/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.rmsoft.assignment.controller;
+
+import com.rmsoft.assignment.service.MemberService;
+import com.rmsoft.assignment.request.MemberSaveRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/save")
+    public void saveMember(@RequestBody @Valid MemberSaveRequest request) {
+        memberService.saveMember(request);
+    }
+
+}
