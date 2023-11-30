96dba0faccee4ee9bb1132b4cb7ef2cf300aabd6
CMHyuk <alsgur9710@naver.com>
2023-11-30 21:35:00 +0900

    feat : 회원 비즈니스 로직 - 회원가입

---
+++ src/main/java/com/rmsoft/assignment/service/MemberService.java
@@ -0,0 +1,35 @@
+package com.rmsoft.assignment.service;
+
+import com.rmsoft.assignment.entity.Member;
+import com.rmsoft.assignment.exception.AlreadyExistsEmailException;
+import com.rmsoft.assignment.repository.MemberMapper;
+import com.rmsoft.assignment.request.MemberSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void saveMember(MemberSaveRequest request) {
+        Optional<Member> memberOptional = memberMapper.findByEmail(request.getEmail());
+
+        //이메일 중복 방지
+        if (memberOptional.isPresent()) {
+            throw new AlreadyExistsEmailException();
+        }
+
+        String encryptedPassword = passwordEncoder.encode(request.getPassword()); //비밀번호 암호화
+        Member member = new Member(request.getEmail(), encryptedPassword);
+        memberMapper.save(member);
+    }
+
+}
