c8191a13ce91e298fa176881d40903e81e486d01
CMHyuk <alsgur9710@naver.com>
2023-12-02 17:11:36 +0900

    feat : 대출 상태 변경 mapper

--- src/main/java/com/rmsoft/assignment/repository/BookMapper.java
+++ src/main/java/com/rmsoft/assignment/repository/BookMapper.java
@@ -3,12 +3,14 @@ package com.rmsoft.assignment.repository;
 import com.rmsoft.assignment.entity.Book;
 import com.rmsoft.assignment.request.BookUpdateRequest;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.Optional;
 
 @Mapper
 public interface BookMapper {
     void saveBook(Book book);
-    void updateBook(Long bookId, BookUpdateRequest request);
+    void updateBook(@Param("bookId") Long bookId, @Param("bookUpdateRequest") BookUpdateRequest bookUpdateRequest);
+    void updateLoanStatus(Long bookId, Boolean isLoan);
     Optional<Book> findById(Long bookId);
 }
