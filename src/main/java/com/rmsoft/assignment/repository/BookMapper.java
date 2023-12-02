c97ada07388631153b7f6c077a26c1c82401cb3f
CMHyuk <alsgur9710@naver.com>
2023-12-02 23:04:21 +0900

    refactor : updateBook 파라미터 수정

--- src/main/java/com/rmsoft/assignment/repository/BookMapper.java
+++ src/main/java/com/rmsoft/assignment/repository/BookMapper.java
@@ -3,14 +3,13 @@ package com.rmsoft.assignment.repository;
 import com.rmsoft.assignment.entity.Book;
 import com.rmsoft.assignment.request.BookUpdateRequest;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.Optional;
 
 @Mapper
 public interface BookMapper {
     void saveBook(Book book);
-    void updateBook(@Param("bookId") Long bookId, @Param("bookUpdateRequest") BookUpdateRequest bookUpdateRequest);
+    void updateBook(BookUpdateRequest bookUpdateRequest);
     void updateLoanStatus(Long bookId, Boolean isLoan);
     Optional<Book> findById(Long bookId);
 }
