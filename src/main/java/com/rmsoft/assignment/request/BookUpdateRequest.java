9f0755455744ac8199f7cda1af2e949c86f449af
CMHyuk <alsgur9710@naver.com>
2023-12-02 17:09:53 +0900

    refactor : 쓰지 않는 필드 제거

--- src/main/java/com/rmsoft/assignment/request/BookUpdateRequest.java
+++ src/main/java/com/rmsoft/assignment/request/BookUpdateRequest.java
@@ -1,11 +1,9 @@
 package com.rmsoft.assignment.request;
 
-import com.rmsoft.assignment.entity.LoanStatus;
 import lombok.Getter;
 
 @Getter
 public class BookUpdateRequest {
     private String bookName;
     private String isbn;
-    private LoanStatus loanStatus;
 }
