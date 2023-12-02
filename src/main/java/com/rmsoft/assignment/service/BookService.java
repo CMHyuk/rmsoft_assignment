c280b1cfe91b1da794c2982b258c241b0574916d
CMHyuk <alsgur9710@naver.com>
2023-12-02 17:09:36 +0900

    refactor : enum -> Boolean 타입으로 변경으로인한 코드 변경 & 예외처리 추가

--- src/main/java/com/rmsoft/assignment/service/BookService.java
+++ src/main/java/com/rmsoft/assignment/service/BookService.java
@@ -3,6 +3,8 @@ package com.rmsoft.assignment.service;
 import com.rmsoft.assignment.entity.Book;
 import com.rmsoft.assignment.entity.Member;
 import com.rmsoft.assignment.exception.BookNotFound;
+import com.rmsoft.assignment.exception.BookNotLoanedException;
+import com.rmsoft.assignment.exception.DuplicateLoanException;
 import com.rmsoft.assignment.exception.MemberNotFound;
 import com.rmsoft.assignment.repository.BookMapper;
 import com.rmsoft.assignment.repository.MemberMapper;
@@ -12,8 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.rmsoft.assignment.entity.LoanStatus.INACTIVE;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -21,6 +21,7 @@ public class BookService {
 
     private final BookMapper bookMapper;
     private final MemberMapper memberMapper;
+    private final LoanHistoryService loanHistoryService;
 
     //도서 등록
     @Transactional
@@ -28,35 +29,52 @@ public class BookService {
         Book book = Book.builder()
                 .bookName(request.getBookName())
                 .isbn(request.getIsbn())
-                .loanStatus(INACTIVE) //처음 도서 등록할 때는 대출이 아닌 상태
+                .isLoan(false) //처음 도서 등록할 때는 대출이 아닌 상태
                 .build();
 
         bookMapper.saveBook(book);
     }
 
+    //도서 수정 - 오류 발생
     @Transactional
-    public void updateBook(Long bookId, BookUpdateRequest request) {
+    public void updateBook(Long bookId, BookUpdateRequest bookUpdateRequest) {
         Book book = bookMapper.findById(bookId)
                 .orElseThrow(BookNotFound::new);
 
-        bookMapper.updateBook(book.getBookId(), request);
+        bookMapper.updateBook(book.getBookId(), bookUpdateRequest);
     }
 
     //도서 대출
     @Transactional
-    public void borrowBook(Long bookId, String email) {
+    public void borrowBook(Long bookId, String email, Boolean isLoan) {
         Member member = memberMapper.findByEmail(email)
                 .orElseThrow(MemberNotFound::new);
 
         Book book = bookMapper.findById(bookId)
                 .orElseThrow(BookNotFound::new);
+
+        //이미 대출된 도서 대출 시 에러 발생
+        if (book.getIsLoan()) {
+            throw new DuplicateLoanException();
+        }
+
+        bookMapper.updateLoanStatus(bookId, isLoan);
+        loanHistoryService.saveLoanHistory(bookId, email);
     }
 
     //도서 반납
     @Transactional
-    public void returnBook(Long bookId) {
+    public void returnBook(Long bookId, String email, Boolean isLoan) {
         Book book = bookMapper.findById(bookId)
                 .orElseThrow(BookNotFound::new);
+
+        //대출하지 않는 도서를 반납하려고 할 때
+        if (!book.getIsLoan()) {
+            throw new BookNotLoanedException();
+        }
+
+        bookMapper.updateLoanStatus(bookId, isLoan);
+        loanHistoryService.saveLoanHistory(bookId, email);
     }
 
 }
