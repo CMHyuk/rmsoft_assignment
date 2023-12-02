f2eb8f4af10daa7bd20337ac37924253c3a39fcf
CMHyuk <alsgur9710@naver.com>
2023-12-02 23:02:55 +0900

    feat : bookId 추가

--- src/main/java/com/rmsoft/assignment/request/BookUpdateRequest.java
+++ src/main/java/com/rmsoft/assignment/request/BookUpdateRequest.java
@@ -4,6 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class BookUpdateRequest {
+
+    private Long bookId;
     private String bookName;
     private String isbn;
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
 }
