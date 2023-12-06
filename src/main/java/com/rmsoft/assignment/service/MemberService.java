443f0a684f5e4bbb0b5fde201fd241c7faf3839d
CMHyuk <alsgur9710@naver.com>
2023-12-06 13:59:45 +0900

    feat : 비밀번호, 비밀번호 확인 일치 검증

--- src/main/java/com/rmsoft/assignment/service/MemberService.java
+++ src/main/java/com/rmsoft/assignment/service/MemberService.java
@@ -2,6 +2,7 @@ package com.rmsoft.assignment.service;
 
 import com.rmsoft.assignment.entity.Member;
 import com.rmsoft.assignment.exception.member.AlreadyExistsEmailException;
+import com.rmsoft.assignment.exception.member.PasswordMismatchException;
 import com.rmsoft.assignment.repository.MemberMapper;
 import com.rmsoft.assignment.request.MemberSaveRequest;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +26,11 @@ public class MemberService {
         //이메일 중복 방지
         if (memberOptional.isPresent()) {
             throw new AlreadyExistsEmailException();
+        }
+
+        //비밀번호, 비밀번호 확인 일치 확인
+        if (!request.getPassword().equals(request.getPasswordConfirm())) {
+            throw new PasswordMismatchException();
         }
 
         String encryptedPassword = passwordEncoder.encode(request.getPassword()); //비밀번호 암호화
