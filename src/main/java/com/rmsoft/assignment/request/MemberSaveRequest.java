d26cb6b3e9bd875830bded6a62e3c51f3144602f
CMHyuk <alsgur9710@naver.com>
2023-12-06 13:52:16 +0900

    refactor : 비밀번호 입력 에러 메시지 수정

--- src/main/java/com/rmsoft/assignment/request/MemberSaveRequest.java
+++ src/main/java/com/rmsoft/assignment/request/MemberSaveRequest.java
@@ -10,7 +10,7 @@ public class MemberSaveRequest {
     @Email(message = "이메일 형식으로 작성해주세요")
     private String email;
 
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상, 특수문자를 포함해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상, 숫자와 특수문자를 포함해야 합니다.")
     private String password;
 
     private String passwordConfirm;
